d44b31ad421c77b4df4a2377cf931b51da7aea65
J-Andrew4852 <james.andrew4852@gmail.com>
2022-11-09 23:00:21 +0800

    favicon commit

--- pages/_app.js
+++ pages/_app.js
@@ -5,6 +5,9 @@ import { Layout } from '../components/layout/Layout'
 function MyApp({ Component, pageProps }) {
   return (
     <Layout>
+      <Head>
+        <link rel="shortcut icon" href="..public/images/favicon.ico" />
+      </Head>
       <Component {...pageProps} />
     </Layout>
   )
