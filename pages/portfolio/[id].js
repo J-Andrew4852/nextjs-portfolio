2c2d3de9b0dac576fa34232fd986ca3ff872e5b3
J-Andrew4852 <james.andrew4852@gmail.com>
2022-11-23 19:16:27 +0800

    favicon

--- pages/portfolio/[id].js
+++ pages/portfolio/[id].js
@@ -11,7 +11,11 @@ export default function ProjectmDetail({ project }) {
         <div className={`${styles.projectPageContainer} ${styles.contianer}`}>
             <Head>
                 <title>Project Details | James Andrew Digital</title>
-                <link rel="shortcut icon" href="/favicon.ico" />
+                <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
+                <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
+                <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
+                <link rel="manifest" href="/site.webmanifest"/>
+                <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5"/>
             </Head>
 
             <PageTitleHeader title={project.title} navigation={true}/>
--- pages/portfolio/index.js
+++ pages/portfolio/index.js
@@ -18,7 +18,11 @@ export default function Portfolio({ portfolio }) {
     <div className={styles.portfolioPage}>
       <Head>
         <title>Portfolio | James Andrew Digital</title>
-        <link rel="shortcut icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
+        <link rel="manifest" href="/site.webmanifest"/>
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5"/>
       </Head>
 
       <PageTitleHeader title="My Portfolio" navigation={false}/>
